4d432e4d17e0a43fe01c4d82dacb975a81656fc4
Ovardov <ovardov7@gmail.com>
2019-11-11 18:03:55 +0200

    Initial commit

---
+++ Client/src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.site}>
+      
+    </div>
+  );
+}
+
+export default App;
