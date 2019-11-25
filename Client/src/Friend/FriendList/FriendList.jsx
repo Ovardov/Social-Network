4ef9ab2978ba3e372368b753de0c228cf02ead0e
Ovardov <ovardov7@gmail.com>
2019-11-25 18:25:22 +0200

    Add frind page

---
+++ Client/src/Friend/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import FriendCard from '../FriendCard/FriendCard';
+import styles from './friend-list.module.scss';
+
+function renderPosts(friends) {
+    return friends.map(friend => {
+        return (<FriendCard key={friend.id} {...friend} />);
+    });
+}
+
+function FriendList({ friends }) {
+    return (
+        <div className={styles['friends-container']}>
+            {renderPosts(friends)}
+        </div>
+    )
+}
+
+export default FriendList;
