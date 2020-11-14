0dc25f21dfb1d9635a10e374a6ec9d27ec4dd80a
Ovardov <ovardov7@gmail.com>
2020-11-14 17:33:36 +0200

    Maked login page responsive

--- Client/components/PublicHome/index.jsx
+++ Client/components/PublicHome/index.jsx
@@ -9,14 +9,14 @@ const PublicHome = ({ children }) => {
         <div className={styles['image-container']}>
           <Image layout="fill" loading="lazy" src="/images/home-page.svg" alt="Friends" />
         </div>
-
-        <ul className={styles.links}>
-          <li><Link href='/login'><a  className={`${styles.link} ${styles.selected}`}>Login</a></Link></li>
-          <li><Link href='/register'><a  className={`${styles.link}`}>Register</a></Link></li>
-        </ul>
       </article>
 
       <article className={styles.data}>
+        <ul className={styles.links}>
+          <li><Link href='/login'><a className={`${styles.link} ${styles.selected}`}>Login</a></Link></li>
+          <li><Link href='/register'><a className={`${styles.link}`}>Register</a></Link></li>
+        </ul>
+        
         {children}
       </article>
     </section>
