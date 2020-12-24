26fb7cc555382f6ef940d83678e296b8f94a41c4
Ovardov <ovardov7@gmail.com>
2020-12-24 21:56:37 +0200

    Added disabled attribute in buttons

--- ReactClient/src/components/Global/Buttons/Button/index.jsx
+++ ReactClient/src/components/Global/Buttons/Button/index.jsx
@@ -4,24 +4,31 @@ import PropTypes from 'prop-types'
 // Styles
 import globalButtonStyles from '../buttons.module.scss'
 
-const Button = ({ type, text, color, onClickHandler, icon }) => {
-  const buttonOnClickHandler = onClickHandler ? { onClick : onClickHandler } : null;
+const Button = ({ type, disabled, text, color, onClickHandler }) => {
+  const buttonOnClickHandler = onClickHandler
+    ? { onClick: onClickHandler }
+    : null
 
   return (
-    <button type={type} className={`${globalButtonStyles.button} ${globalButtonStyles[`color-${color}`]}`}  {...buttonOnClickHandler}>
+    <button
+      type={type}
+      disabled={disabled}
+      className={`${globalButtonStyles.button} ${
+        globalButtonStyles[`color-${color}`]
+      }`}
+      {...buttonOnClickHandler}
+    >
       {text}
-
-      {icon && <span className={globalButtonStyles.icon}></span>}
     </button>
   )
 }
 
 Button.propTypes = {
   type: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['primary', 'secondary']).isRequired,
   onClickHandler: PropTypes.func,
-  icon: PropTypes.string
 }
 
 export default Button
