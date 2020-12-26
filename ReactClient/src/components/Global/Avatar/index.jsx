a2a39e562e2167d0c3026497ec3bc64e3075078e
Ovardov <ovardov7@gmail.com>
2020-12-26 12:22:03 +0200

    Added default profile picture if user do not have

--- ReactClient/src/components/Global/Avatar/index.jsx
+++ ReactClient/src/components/Global/Avatar/index.jsx
@@ -1,16 +1,25 @@
 // Libraries
 import React from 'react'
 import PropTypes from 'prop-types'
+// Images
+import defaultProfilePicture from '../../../../public/images/profile-placeholder.png'
 // Styles
 import styles from './index.module.scss'
 
 const Avatar = ({ size, imageSrc, imageAlt }) => {
-  return <img className={`${styles.avatar} ${styles[`size-${size}`]}`} src={imageSrc} alt={imageAlt} />
+  return (
+    <img
+      className={`${styles.avatar} ${styles[`size-${size}`]}`}
+      // If user do not have profile picture we showed default
+      src={imageSrc ? imageSrc : defaultProfilePicture}
+      alt={imageAlt}
+    />
+  )
 }
 
 Avatar.propTypes = {
   size: PropTypes.oneOf(['sm', 'md', 'lg']).isRequired,
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string.isRequired,
 }
 
